43cc61bdf4f0134b7eda05eac6da30b1b81de5d8
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-12 11:11:56 -0600

    add test for word constructor, pass

--- Anagram.Tests/ModelTests/WordTests.cs
+++ Anagram.Tests/ModelTests/WordTests.cs
@@ -1,1 +1,24 @@
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagram.Models;
+
+namespace Anagram.Tests
+{
+  [TestClass]
+  public class WordTests //: IDisposable
+  {
+    // public void Dispose()
+    // {
+    //   Word.ClearAll();
+    // }
+
+    [TestMethod]
+    public void WordConstructor_CreatesInstanceOfWord_Word()
+    {
+      Word newWord = new Word();
+      Assert.AreEqual(typeof(Word), newWord.GetType());
+    }
+
+
+  }
+}
